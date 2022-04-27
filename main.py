15789e1a62c38d3d59710fe87616b14b5a4e5b75
Rubén Juárez <91731294+rubences@users.noreply.github.com>
2022-04-27 15:00:55 +0000

    Primer caso

--- main.py
+++ main.py
@@ -22,10 +22,48 @@ def wget(uri):
         response = conn.getresponse()    
         # Análisis de la respuesta    
         if response.status != 200:    
-            # 200 = Ok, 3xx = redirection, 4xx = error client,   
+            # 200 = Ok, 3xx = redirection, 4xx = error client,  
 5xx = error servidor    
             print(response.reason, file=stderr)    
             return    
         # Devuelve la respuesta si todo está OK.    
         print('Respuesta OK')    
         return response.read() 
+
+def get_images_src_from_html(html_doc):    
+    """Recupera toda el contenido de los atributos src de las etiquetas img""" 
+    soup = BeautifulSoup(html_doc, "html.parser")    
+    return [img.get('src') for img in soup.find_all('img')] 
+
+def get_uri_from_images_src(base_uri, images_src):    
+    """Devuelve una a una cada URI de imagen a descargar"""    
+    parsed_base = urlparse(base_uri)    
+    result = []    
+    for src in images_src:    
+        parsed = urlparse(src)    
+        if parsed.netloc == '':    
+            path = parsed.path    
+            if parsed.query:    
+                path += '?' + parsed.query    
+            if path[0] != '/':    
+                if parsed_base.path == '/':    
+                    path = '/' + path    
+                else:    
+                    path = '/' + '/'.join(parsed_base.path.split('/') 
+[:-1]) + '/' + path    
+            result.append(parsed_base.scheme + '://' +   
+parsed_base.netloc + path)    
+        else:    
+            result.append(parsed.geturl())    
+    return result 
+
+def download(uri):    
+    """    
+    Guarda en el disco duro un archivo designado por una URI   
+    """    
+    content = wget(uri)    
+    if content is None:    
+        return None    
+    with open(uri.split(sep)[-1], 'wb') as f:    
+        f.write(content)    
+        return uri 
